696481caf5a27d7b0bf3e8cc18b0331e593e2d7b
Mohammad Haider <lordxgrimx786@gmail.com>
2025-08-11 12:13:26 +0530

    done

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,40 +1,45 @@
-import {combineReducers, configureStore} from "@reduxjs/toolkit";
+// store.js
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice.js";
 import messageReducer from "./messageSlice.js";
 import socketReducer from "./socketSlice.js";
 import {
-    persistReducer,
-    FLUSH,
-    REHYDRATE,
-    PAUSE,
-    PERSIST,
-    PURGE,
-    REGISTER,
-  } from 'redux-persist';
-  import storage from 'redux-persist/lib/storage'
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-  const persistConfig = {
-    key: 'root',
-    version: 1,
-    storage,
-  }
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
 
-  const rootReducer = combineReducers({
-    user:userReducer,
-    message:messageReducer,
-    socket:socketReducer
- })
+const rootReducer = combineReducers({
+  user: userReducer,
+  message: messageReducer,
+  socket: socketReducer,
+});
 
-const persistedReducer = persistReducer(persistConfig, rootReducer)
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-
-const store = configureStore({
-    reducer:persistedReducer,
-    middleware: (getDefaultMiddleware) =>
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
     }),
 });
+
+// Add this line so persistence actually works
+export const persistor = persistStore(store);
+
 export default store;
